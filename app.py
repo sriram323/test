14edf05aa4b9435a3dc3718c3cf571d373545083
root <root@ip-172-31-17-168.us-east-2.compute.internal>
2018-07-16 14:24:55 +0000

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker(Very Excited test)!!"
+    return "Flask inside Docker(Very Excited about the container)!!"
 
 
 if __name__ == "__main__":
