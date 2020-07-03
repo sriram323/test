a1f6cb7c5d9b0cfccc536fea49ddc27dbc1866a4
sriram323 <sriram01.cse@gmail.com>
2020-07-03 15:08:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ def hello():
     return "Flask inside Docker(First container in pipeline)!!"
 DB_PAssword = "s488323@swe@3$%#"
 
+DB = "s488323@swe@3$%#"
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
