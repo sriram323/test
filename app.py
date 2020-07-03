7fd32243c228919079ac9c87a4c4922150bfbf7c
sriram323 <sriram01.cse@gmail.com>
2020-07-03 16:43:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ def hello():
     return "Flask inside Docker(First container in pipeline)!!"
 DB_PAssword = "s488323@swe@3$%#"
 
+DB = "s488323@swe@3$%#"  
 DB = "s488323@swe@3$%#"
+
 
 
 if __name__ == "__main__":
